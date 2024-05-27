f279473e5a9521c6cd2fb830704efec28f858333
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-27 20:53:29 -0300

    feat: Deve retornar erro se userId não existe

--- src/tests/unit/create-cart.service.test.ts
+++ src/tests/unit/create-cart.service.test.ts
@@ -2,20 +2,32 @@ import assert from 'assert';
 import CreateCartService from '../../services/create-cart.service';
 import CartRepository from '../../repositories/cart-repository';
 import Sinon from 'sinon';
+import AxiosClient from '../../infra/axios-client';
+import envs from '../../config/global';
 
 const mocks = {
   returnCartSuccess: require('../mocks/return-cart-success'),
+  returnUserSuccess: require('../mocks/return-user-success.json'),
 };
 
 describe('Create Cart Service', () => {
   const cartRepository = new CartRepository();
-  const createCartService = new CreateCartService(cartRepository);
+  const axiosClient = new AxiosClient(envs.API_URL);
+  const createCartService = new CreateCartService(cartRepository, axiosClient);
 
   afterEach(() => {
     Sinon.restore();
   });
 
   it('Deve criar carrinho com o userId e status.', async () => {
+    Sinon.stub(AxiosClient.prototype, 'get').resolves({
+      data: mocks.returnUserSuccess,
+      status: 0,
+      statusText: '',
+      headers: undefined,
+      config: undefined,
+    });
+
     Sinon.stub(CartRepository.prototype, 'create').resolves(
       mocks.returnCartSuccess,
     );
@@ -26,5 +38,26 @@ describe('Create Cart Service', () => {
 
     const result = await createCartService.create(data);
     assert.deepEqual(result, mocks.returnCartSuccess);
+  });
+
+  it('Deve retornar erro se userId não existe.', async () => {
+    Sinon.stub(AxiosClient.prototype, 'get').resolves({
+      data: null,
+      status: 0,
+      statusText: '',
+      headers: undefined,
+      config: undefined,
+    });
+
+    const data = {
+      userID: 'a6fcbddd-cb9d-4d75-acb3-105a50a607e2',
+    };
+
+    const promise = createCartService.create(data);
+    await assert.rejects(promise, (error: any) => {
+      assert.deepEqual(error.status, 401);
+      assert.deepEqual(error.message, 'Missing param Usuário Invalido');
+      return true;
+    });
   });
 });
