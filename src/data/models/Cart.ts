ca6620592217f6eef8dfed406547068693137180
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-27 19:39:07 -0300

    cria model sequelize para cart

---
+++ src/data/models/Cart.ts
@@ -0,0 +1,36 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../../config/sequelize';
+
+class Cart extends Model {
+  public id!: string;
+  public userID!: string;
+  public status!: string;
+}
+
+Cart.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    userID: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'active',
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Cart',
+    tableName: 'carts',
+  },
+);
+
+Cart.belongsTo(sequelize.models.User, { foreignKey: 'userID' });
+
+export default Cart;
