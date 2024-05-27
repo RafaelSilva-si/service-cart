96485ccdf6036f1474099b17e6861777a1507d5e
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-27 20:53:13 -0300

    Deve retornar erro se userId não existe

--- src/services/create-cart.service.ts
+++ src/services/create-cart.service.ts
@@ -1,14 +1,22 @@
 import Cart from '../domain/model/Cart';
 import { CreateCart } from '../domain/usecases/create-cart';
+import AxiosClient from '../infra/axios-client';
 import CartRepository from '../repositories/cart-repository';
+import { MissingParamError } from '../utils/errors/missingParamError';
 
 class CreateCartService implements CreateCart {
   private readonly cartRepository: CartRepository;
-  constructor(cartRepository: CartRepository) {
+  private readonly axiosClient: AxiosClient;
+
+  constructor(cartRepository: CartRepository, axiosClient: AxiosClient) {
     this.cartRepository = cartRepository;
+    this.axiosClient = axiosClient;
   }
 
   async create(data: any): Promise<Cart> {
+    const user = await this.axiosClient.get('/user');
+    if (!user.data) throw new MissingParamError('Usuário Invalido', 401);
+
     return await this.cartRepository.create(data);
   }
 }
