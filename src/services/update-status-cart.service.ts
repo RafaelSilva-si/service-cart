6e4ff6691f7b538b1e2c3bbb750a1f6c76c2c3f6
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-28 21:26:41 -0300

    feat: deve atualizar o status de um cart

---
+++ src/services/update-status-cart.service.ts
@@ -0,0 +1,21 @@
+import Cart from '../domain/model/Cart';
+import { UpdateStatusCart } from '../domain/usecases/update-status-cart';
+import CartRepository from '../repositories/cart-repository';
+import { MissingParamError } from '../utils/errors/missingParamError';
+
+class UpdateStatusCartService implements UpdateStatusCart {
+  private readonly cartRepository: CartRepository;
+
+  constructor(cartRepository: CartRepository) {
+    this.cartRepository = cartRepository;
+  }
+
+  async update(id: string, status: string): Promise<Cart> {
+    const existCart = await this.cartRepository.getCartById(id);
+    if (!existCart) throw new MissingParamError('Carrinho Não Existe', 401);
+
+    return await this.cartRepository.update(id, status);
+  }
+}
+
+export default UpdateStatusCartService;
