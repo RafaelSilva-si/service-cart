c7d059a210e00da1e647f89c2c373d29036b5852
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-29 20:51:40 -0300

    test: Deve retornar erro se o item não existir

--- src/tests/unit/cart-items/remove-item-to-cart.service.test.ts
+++ src/tests/unit/cart-items/remove-item-to-cart.service.test.ts
@@ -3,6 +3,10 @@ import CartItemsRepository from '../../../repositories/cart-items.repository';
 import RemoveItemToCartService from '../../../services/cart-items/remove-item-to-cart.service';
 import Sinon from 'sinon';
 
+const mocks = {
+  returnCartItemSuccess: require('../../mocks/return-cart-item-success.json'),
+};
+
 describe('Remover Item do Carrinho', () => {
   const cartItemRepository = new CartItemsRepository();
   const removeItemToCartService = new RemoveItemToCartService(
@@ -14,6 +18,9 @@ describe('Remover Item do Carrinho', () => {
   });
 
   it('Deve remover um item existente do carrinho', async () => {
+    Sinon.stub(CartItemsRepository.prototype, 'getItemByID').resolves(
+      mocks.returnCartItemSuccess,
+    );
     Sinon.stub(CartItemsRepository.prototype, 'removeItem').resolves(true);
 
     const result = await removeItemToCartService.removeItem(
@@ -21,5 +28,18 @@ describe('Remover Item do Carrinho', () => {
     );
 
     assert.deepEqual(result, true);
+  });
+
+  it('Deve retornar erro se o item não existir', async () => {
+    Sinon.stub(CartItemsRepository.prototype, 'getItemByID').resolves(null);
+
+    const promise = removeItemToCartService.removeItem(
+      'a6fcbddd-cb9d-4d75-acb3-105a50a607e2',
+    );
+
+    await assert.rejects(promise, {
+      message: 'Missing param Item inexistente',
+      status: 401,
+    });
   });
 });
