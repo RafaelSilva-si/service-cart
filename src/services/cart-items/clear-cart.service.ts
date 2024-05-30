e1fc01c101fc0512d5e9aa4f812a30f2b33439e2
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-29 21:34:08 -0300

    feat: Deve retornar erro se o carrinho não existir

--- src/services/cart-items/clear-cart.service.ts
+++ src/services/cart-items/clear-cart.service.ts
@@ -1,14 +1,24 @@
 import ClearCart from '../../domain/usecases/cart-items/clear-cart';
 import CartItemsRepository from '../../repositories/cart-items.repository';
+import CartRepository from '../../repositories/cart-repository';
+import { MissingParamError } from '../../utils/errors/missingParamError';
 
 class ClearCartService implements ClearCart {
   private readonly cartItemsRepository: CartItemsRepository;
+  private readonly cartRepository: CartRepository;
 
-  constructor(cartItemsRepository: CartItemsRepository) {
+  constructor(
+    cartItemsRepository: CartItemsRepository,
+    cartRepository: CartRepository,
+  ) {
     this.cartItemsRepository = cartItemsRepository;
+    this.cartRepository = cartRepository;
   }
 
   async clear(cartID: string): Promise<boolean> {
+    const existCart = await this.cartRepository.getCartById(cartID);
+    if (!existCart) throw new MissingParamError('Carrinho não existe', 404);
+
     return await this.cartItemsRepository.clear(cartID);
   }
 }
