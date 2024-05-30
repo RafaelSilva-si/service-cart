987817d0b096fcbd094633b887c031975b23da15
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-29 21:34:20 -0300

    test: Deve retornar erro se o carrinho não existir

--- src/tests/unit/cart-items/clear-cart.service.test.ts
+++ src/tests/unit/cart-items/clear-cart.service.test.ts
@@ -2,10 +2,19 @@ import assert from 'assert';
 import CartItemsRepository from '../../../repositories/cart-items.repository';
 import ClearCartService from '../../../services/cart-items/clear-cart.service';
 import Sinon from 'sinon';
+import CartRepository from '../../../repositories/cart-repository';
+
+const mocks = {
+  returnCartSuccess: require('../../mocks/return-cart-success'),
+};
 
 describe('Limpar carrinho', () => {
   const cartItemRepository = new CartItemsRepository();
-  const clearCartService = new ClearCartService(cartItemRepository);
+  const cartRepository = new CartRepository();
+  const clearCartService = new ClearCartService(
+    cartItemRepository,
+    cartRepository,
+  );
 
   afterEach(() => {
     Sinon.restore();
@@ -13,10 +22,26 @@ describe('Limpar carrinho', () => {
 
   it('Deve remover todos items do carrinho', async () => {
     Sinon.stub(CartItemsRepository.prototype, 'clear').resolves(true);
+    Sinon.stub(CartRepository.prototype, 'getCartById').resolves(
+      mocks.returnCartSuccess,
+    );
 
     const result = await clearCartService.clear(
       'a6fcbddd-cb9d-4d75-acb3-105a50a607e2',
     );
     assert.deepEqual(result, true);
+  });
+
+  it('Deve retornar erro se o carrinho não existir', async () => {
+    Sinon.stub(CartRepository.prototype, 'getCartById').resolves(null);
+
+    const promise = clearCartService.clear(
+      'a6fcbddd-cb9d-4d75-acb3-105a50a607e2',
+    );
+
+    await assert.rejects(promise, {
+      message: 'Missing param Carrinho não existe',
+      status: 404,
+    });
   });
 });
