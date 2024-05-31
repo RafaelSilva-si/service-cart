37a9916c18883a8719d237b24df75be7210dede3
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-31 15:21:43 -0300

    test: refatora teste para novas interfaces

--- src/tests/unit/cart-items/add-item-to-cart.service.test.ts
+++ src/tests/unit/cart-items/add-item-to-cart.service.test.ts
@@ -3,20 +3,26 @@ import CartItemsRepository from '../../../repositories/cart-items.repository';
 import AddItemToCartService from '../../../services/cart-items/add-item-to-cart.service';
 import Sinon from 'sinon';
 import CartRepository from '../../../repositories/cart-repository';
+import AxiosClient from '../../../infra/axios-client';
+import envs from '../../../config/global';
 
 const mocks = {
   returnCartSuccess: require('../../mocks/return-cart-success'),
   returnUserSuccess: require('../../mocks/return-user-success.json'),
   returnCartItemSuccess: require('../../mocks/return-cart-item-success.json'),
   returnCartClosed: require('../../mocks/return-cart-closed.json'),
+  returnSuccessEvent: require('../../mocks/return-success-event.json'),
 };
 
 describe('Add Item To Cart', () => {
   const cartItemRepository = new CartItemsRepository();
   const cartRepository = new CartRepository();
+  const axiosClient = new AxiosClient(envs.API_URL);
+
   const addItemToCartService = new AddItemToCartService(
     cartItemRepository,
     cartRepository,
+    axiosClient,
   );
 
   afterEach(() => {
@@ -28,13 +34,26 @@ describe('Add Item To Cart', () => {
       mocks.returnCartSuccess,
     );
 
+    Sinon.stub(AxiosClient.prototype, 'get').resolves({
+      data: mocks.returnUserSuccess,
+    });
+
     Sinon.stub(CartItemsRepository.prototype, 'addItemToCart').resolves(
       mocks.returnCartItemSuccess,
     );
 
     const data = {
       userID: 'a6fcbddd-cb9d-4d75-acb3-105a50a607e2',
-      itemID: '9152fc3d-0c5a-4ac3-83fe-64bd83b5307c',
+      item: {
+        _id: '665a0d0bb6409a4984e7ce07',
+        title: 'Evento Teste',
+        date: '2024-06-01T12:00:00.000Z',
+        description: 'Descrição do evento teste',
+        category: 'Categoria do Evento',
+        cover: 'teste',
+        location: 'Expo SP',
+        price: 50,
+      },
       qtd: 1,
     };
 
@@ -47,7 +66,16 @@ describe('Add Item To Cart', () => {
 
     const data = {
       userID: 'a6fcbddd-cb9d-4d75-acb3-105a50a607e2',
-      itemID: 'a6fcbddd-cb9d-4d75-acb3-105a50a607e2',
+      item: {
+        _id: '665a0d0bb6409a4984e7ce07',
+        title: 'Evento Teste',
+        date: '2024-06-01T12:00:00.000Z',
+        description: 'Descrição do evento teste',
+        category: 'Categoria do Evento',
+        cover: 'teste',
+        location: 'Expo SP',
+        price: 50,
+      },
       cartID: 'a6fcbddd-cb9d-4d75-acb3-105a50a607e2',
       qtd: 1,
     };
@@ -66,7 +94,16 @@ describe('Add Item To Cart', () => {
 
     const data = {
       userID: 'a6fcbddd-cb9d-4d75-acb3-105a50a607e2',
-      itemID: 'a6fcbddd-cb9d-4d75-acb3-105a50a607e2',
+      item: {
+        _id: '665a0d0bb6409a4984e7ce07',
+        title: 'Evento Teste',
+        date: '2024-06-01T12:00:00.000Z',
+        description: 'Descrição do evento teste',
+        category: 'Categoria do Evento',
+        cover: 'teste',
+        location: 'Expo SP',
+        price: 50,
+      },
       cartID: 'a6fcbddd-cb9d-4d75-acb3-105a50a607e2',
       qtd: 1,
     };
